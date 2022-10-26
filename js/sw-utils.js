cd5c12c1d104eee043941a5dfe632c37e4a5cf28
Javi Perez <javi9876789@gmail.com>
2022-10-25 22:01:48 -0500

    nuevos cambios

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+function actualizaCacheDinamico(dynamicCache, req , res ){
+    if(res.ok){
+        return caches.open(dynamicCache).then(cache=>{
+            cache.put(req,res.clone());
+            return res.clone();
+        })
+    }else{
+        return res;
+    }
+}
